07ed420fc4171c469e8b284a680eb2eedca4d25e
Nikko0809 <47620301+Nikko0809@users.noreply.github.com>
2020-11-19 08:05:32 -0800

    lab 10 part 1

--- storage/select_tables.py
+++ storage/select_tables.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="kafka-nikko.westus2.cloudapp.azure.com", user="user", password="password", database="events")
+db_conn = mysql.connector.connect(host="kafka-acit3855.westus.cloudapp.azure.com", user="user", password="password", database="events")
 
 db_cursor = db_conn.cursor()
 
