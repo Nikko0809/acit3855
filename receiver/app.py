761866b5d8e0457b65c030fcc1b61ccc3d14d355
Nikko0809 <47620301+Nikko0809@users.noreply.github.com>
2020-11-26 02:22:58 -0800

    added nginx

--- receiver/app.py
+++ receiver/app.py
@@ -77,7 +77,7 @@ def accept_order_request(body):
   return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
   app.run(port=8080)
